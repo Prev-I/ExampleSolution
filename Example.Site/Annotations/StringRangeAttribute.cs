4a1d4a0088785d97560df82caf80831d1d1a35b8
Claudio Previtali <claudio.previtali.88@gmail.com>
2019-07-04 11:54:27 +0200

    Added some extented validation Attribute to be used as data annotations in models

---
+++ Example.Site/Annotations/StringRangeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+
+namespace Example.Site.Annotations
+{
+    public class StringRangeAttribute : ValidationAttribute
+    {
+        public string[] AllowedValues { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (AllowedValues?.Contains(value?.ToString()) == true)
+            {
+                return ValidationResult.Success;
+            }
+            if (string.IsNullOrEmpty(ErrorMessage))
+            {
+                ErrorMessage = $"Please enter one of the allowable values: {string.Join(", ", (AllowedValues ?? new string[] { "No allowable values found" }))}.";
+            }
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
---
+++ Example.Site/Annotations/ValidationAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Example.Site.Annotations
+{
+    public class EmailAttribute : RegularExpressionAttribute
+    {
+        public EmailAttribute()
+            : base(GetRegex())
+        { }
+
+        private static string GetRegex()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["validation.Email"];
+        }
+    }
+
+    public class CodFiscaleAttribute : RegularExpressionAttribute
+    {
+        public CodFiscaleAttribute()
+            : base(GetRegex())
+        { }
+
+        private static string GetRegex()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["validation.CodFiscale"];
+        }
+    }
+
+    public class NroCellulareAttribute : RegularExpressionAttribute
+    {
+        public NroCellulareAttribute()
+            : base(GetRegex())
+        { }
+
+        private static string GetRegex()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["validation.NroCellulare"];
+        }
+    }
+}
